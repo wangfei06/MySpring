b5caae077bde0478e32882373165f686d317902b
AirShuttle <wangfei.buaa@foxmail.com>
2023-04-08 17:30:29 +0800

    初步搭建mvc框架

--- src/main/java/cn/spring/web/DispatcherServlet.java
+++ src/main/java/cn/spring/web/DispatcherServlet.java
@@ -17,142 +17,147 @@ import java.util.List;
 import java.util.Map;
 
 /**
- * Servlet implementation class DispatcherServlet
+ * MVC的核心启动类是DispatchServlet，是Servlet的一种实现
  */
 public class DispatcherServlet extends HttpServlet {
-	private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
     private String sContextConfigLocation;
     private List<String> packageNames = new ArrayList<>();
-    private Map<String,Object> controllerObjs = new HashMap<>();
+    private Map<String, Object> controllerObjs = new HashMap<>();
     private List<String> controllerNames = new ArrayList<>();
-    private Map<String,Class<?>> controllerClasses = new HashMap<>();
-    
+    private Map<String, Class<?>> controllerClasses = new HashMap<>();
+
     private List<String> urlMappingNames = new ArrayList<>();
-    private Map<String,Object> mappingObjs = new HashMap<>();
-    private Map<String,Method> mappingMethods = new HashMap<>();
+    private Map<String, Object> mappingObjs = new HashMap<>();
+    private Map<String, Method> mappingMethods = new HashMap<>();
 
     public DispatcherServlet() {
         super();
     }
-    
+
     @Override
     public void init(ServletConfig config) throws ServletException {
-    	super.init(config);
-    	
+        super.init(config);
+
         sContextConfigLocation = config.getInitParameter("contextConfigLocation");
-        
         URL xmlPath = null;
-		try {
-			xmlPath = this.getServletContext().getResource(sContextConfigLocation);
-		} catch (MalformedURLException e) {
-			e.printStackTrace();
-		}
-        
+        try {
+            xmlPath = this.getServletContext().getResource(sContextConfigLocation);
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+
+        //扫描所有package，获取所有类的列表
         this.packageNames = XmlScanComponentHelper.getNodeValue(xmlPath);
-
         Refresh();
-        
     }
-    
-    protected void Refresh() {
-    	initController();
-    	initMapping();
-    }
-    
-    protected void initController() {
-    	this.controllerNames = scanPackages(this.packageNames);
-    	
-    	for (String controllerName : this.controllerNames) {
-    		Object obj = null;
-    		Class<?> clz = null;
 
-			try {
-				clz = Class.forName(controllerName);
-				this.controllerClasses.put(controllerName,clz);
-			} catch (ClassNotFoundException e) {
-				e.printStackTrace();
-			}
-			try {
-				obj = clz.newInstance();
-				this.controllerObjs.put(controllerName, obj);
-			} catch (InstantiationException e) {
-				e.printStackTrace();
-			} catch (IllegalAccessException e) {
-				e.printStackTrace();
-			}
-    	}
+    protected void Refresh() {
+        initController();
+        initMapping();
     }
-    
+
+    protected void initController() {
+        this.controllerNames = scanPackages(this.packageNames);
+
+        for (String controllerName : this.controllerNames) {
+            Object obj = null;
+            Class<?> clz = null;
+
+            try {
+                clz = Class.forName(controllerName);
+                this.controllerClasses.put(controllerName, clz);
+            } catch (ClassNotFoundException e) {
+                e.printStackTrace();
+            }
+
+            try {
+                //通过反射获取对象实例
+                obj = clz.newInstance();
+                this.controllerObjs.put(controllerName, obj);
+            } catch (InstantiationException e) {
+                e.printStackTrace();
+            } catch (IllegalAccessException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
     private List<String> scanPackages(List<String> packages) {
-    	List<String> tempControllerNames = new ArrayList<>();
-    	for (String packageName : packages) {
-    		tempControllerNames.addAll(scanPackage(packageName));
-    	}
-    	return tempControllerNames;
+        List<String> tempControllerNames = new ArrayList<>();
+        for (String packageName : packages) {
+            tempControllerNames.addAll(scanPackage(packageName));
+        }
+        return tempControllerNames;
     }
-    
+
     private List<String> scanPackage(String packageName) {
-    	List<String> tempControllerNames = new ArrayList<>();
-        URL url  =this.getClass().getClassLoader().getResource("/"+packageName.replaceAll("\\.", "/"));
+        List<String> tempControllerNames = new ArrayList<>();
+        URL url = this.getClass().getClassLoader().getResource("/" + packageName.replaceAll("\\.", "/"));
         File dir = new File(url.getFile());
         for (File file : dir.listFiles()) {
-            if(file.isDirectory()){
-            	scanPackage(packageName+"."+file.getName());
-            }else{
-                String controllerName = packageName +"." +file.getName().replace(".class", "");
+            if (file.isDirectory()) {
+                scanPackage(packageName + "." + file.getName());
+            } else {
+                String controllerName = packageName + "." + file.getName().replace(".class", "");
                 tempControllerNames.add(controllerName);
             }
         }
         return tempControllerNames;
     }
-    
+
+    /**
+     * 将controller类中的方法初始化到mappingMap中
+     */
     protected void initMapping() {
-    	for (String controllerName : this.controllerNames) {
-    		Class<?> clazz = this.controllerClasses.get(controllerName);
-    		Object obj = this.controllerObjs.get(controllerName);
-    		Method[] methods = clazz.getDeclaredMethods();
-    		if(methods!=null){
-    			for(Method method : methods){
-    				boolean isRequestMapping = method.isAnnotationPresent(RequestMapping.class);
-    				if (isRequestMapping){
-    					String methodName = method.getName();
-    					String urlmapping = method.getAnnotation(RequestMapping.class).value();
-    					this.urlMappingNames.add(urlmapping);
-    					this.mappingObjs.put(urlmapping, obj);
-    					this.mappingMethods.put(urlmapping, method);
-    				}
-    			}
-    		}
-    	}
+        for (String controllerName : this.controllerNames) {
+            Class<?> clazz = this.controllerClasses.get(controllerName);
+            Object obj = this.controllerObjs.get(controllerName);
+            Method[] methods = clazz.getDeclaredMethods();
+            if (methods != null) {
+                for (Method method : methods) {
+                    //判断方法上是否有@RequestMapping注解
+                    boolean isRequestMapping = method.isAnnotationPresent(RequestMapping.class);
+                    if (isRequestMapping) {
+                        String methodName = method.getName();
+                        String urlmapping = method.getAnnotation(RequestMapping.class).value();
+                        this.urlMappingNames.add(urlmapping);
+                        this.mappingObjs.put(urlmapping, obj);
+                        this.mappingMethods.put(urlmapping, method);
+                    }
+                }
+            }
+        }
     }
 
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		String sPath = request.getServletPath();
-System.out.println(sPath);		
-		if (!this.urlMappingNames.contains(sPath)) {
-			return;
-		}
-		
-		Object obj = null;
-		Object objResult = null;
-		try {
-			Method method = this.mappingMethods.get(sPath);
-			obj = this.mappingObjs.get(sPath);
-			objResult = method.invoke(obj);
-		} catch (SecurityException e) {
-			e.printStackTrace();
-		} catch (IllegalAccessException e) {
-			e.printStackTrace();
-		} catch (IllegalArgumentException e) {
-			e.printStackTrace();
-		} catch (InvocationTargetException e) {
-			e.printStackTrace();
-		}
-		
-		response.getWriter().append(objResult.toString());
-	}
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String sPath = request.getServletPath();
+        System.out.println(sPath);
+        if (!this.urlMappingNames.contains(sPath)) {
+            return;
+        }
 
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		doGet(request, response);
-	}
+        Object obj = null;
+        Object objResult = null;
+        try {
+            Method method = this.mappingMethods.get(sPath);
+            obj = this.mappingObjs.get(sPath);
+            //方法执行，获取服务执行后的结果
+            objResult = method.invoke(obj);
+        } catch (SecurityException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (IllegalArgumentException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        }
+        //结果返回
+        response.getWriter().append(objResult.toString());
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doGet(request, response);
+    }
 }
